7715a03b9675f45a74ac0e6a339e17578841d00d
Ariel Vernaza <dsapandora@Ariels-MacBook-Pro.local>
2024-07-21 16:51:27 -0500

    modify the home to look as a card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import PacmanLoader from 'react-spinners/PacmanLoader';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
   const [loading, setLoading] = useState(false);
   const [avatarURL, setAvatarURL] = useState('');
   const [githubUsername, setGitHubUsername] = useState('');
-  const [repoData, setRepoData] = useState(null);
+  const [repoData, setRepoData] = useState([]);
   const [error, setError] = useState('');
+  const [showRepos, setShowRepos] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -20,23 +22,28 @@ function Home() {
   async function repoDataURL() {
     setLoading(true);
     try {
-      const response = await fetch('https://dsapandora-backend.lazyracoon.tech/pinned-repos/');
+      const response = await fetch('https://dsapandora-backend.lazyracoon.tech/pinned-repos');
       const result = await response.json();
       if (result.error) {
         setError(result.error);
-        setRepoData(null);
+        setRepoData([]);
       } else {
         const { data } = result;
         const { user: { pinnedItems } } = data;
         const list = pinnedItems.edges.map((repo) => {
           const { node: { name, description, url } } = repo;
           return (
-            <div key={url} className="text-center">
-              <a target="_blank" rel="noopener noreferrer" href={url}>
-                {name}
-              </a>
-              <p>Description: {description}</p>
-            </div>
+            <Card key={url} style={{ width: '18rem', margin: '0.5rem' }}>
+              <Card.Img variant="top" src="/lazyracoon_logo.png" />
+              <Card.Body>
+                <Card.Title>
+                  <a target="_blank" rel="noopener noreferrer" href={url}>
+                    {name}
+                  </a>
+                </Card.Title>
+                <Card.Text>Description: {description}</Card.Text>
+              </Card.Body>
+            </Card>
           );
         });
         setRepoData(list);
@@ -45,14 +52,16 @@ function Home() {
     } catch (error) {
       console.error('Error fetching pinned repositories:', error);
       setError('Error fetching pinned repositories');
-      setRepoData(null);
+      setRepoData([]);
     } finally {
       setLoading(false);
+      setShowRepos(true);
     }
   }
 
   function hideRepoData() {
-    setRepoData(null);
+    setRepoData([]);
+    setShowRepos(false);
   }
 
   useEffect(() => {
@@ -70,7 +79,7 @@ function Home() {
   }, []);
 
   return (
-    <div className="home">
+    <div className="home" style={{ padding: '2rem' }}>
       {loading ? (
         <PacmanLoader
           color={"#e66162"}
@@ -80,8 +89,8 @@ function Home() {
           data-testid="loader"
         />
       ) : (
-        <div className="home">
-          <Card style={{ width: '18rem' }}>
+        <div style={{ display: 'flex', flexDirection: showRepos ? 'row' : 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <Card style={{ width: '18rem', marginBottom: '1rem' }}>
             <Card.Img variant="top" src={avatarURL} />
             <Card.Body>
               <Card.Title>{githubUsername}</Card.Title>
@@ -89,7 +98,7 @@ function Home() {
                 I am a Senior Software Engineer, and Artificial Intelligence researcher.
               </Card.Text>
 
-              {repoData ? (
+              {showRepos ? (
                 <Button variant="danger" onClick={hideRepoData}>
                   Hide Projects
                 </Button>
@@ -100,8 +109,12 @@ function Home() {
               )}
             </Card.Body>
           </Card>
-          {repoData}
-          {error && <p>{error}</p>}
+          {showRepos && (
+            <div style={{ display: 'flex', flexWrap: 'wrap', marginLeft: '2rem', justifyContent: 'space-between' }}>
+              {repoData}
+              {error && <p>{error}</p>}
+            </div>
+          )}
         </div>
       )}
     </div>
